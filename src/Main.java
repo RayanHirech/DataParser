704aeaac54c6879f97c6c3846420db855b72e64f
RayanHirech <rhirech059@student.fuhsd.org>
2019-02-28 16:58:52 -0800

    Add test that reads data file and prints it.

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,12 @@
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+
+        System.out.println(data);
+
+    }
+
 }
